468ca2de98cbed816fe476bca74ad5003834da25
Denis <Den2434358@gmail.com>
2021-08-10 13:00:54 +0300

    call data from str to int

--- bot.py
+++ bot.py
@@ -123,8 +123,9 @@ def set_board(message):
 def handle_set_board(call):
     chat_id = call.message.chat.id
     user_data = get_user_data(chat_id)
-    board_id = user_data[call.data]['id']
-    board_name = user_data[call.data]['name']
+    call_data = int(call.data)
+    board_id = user_data[call_data]['id']
+    board_name = user_data[call_data]['name']
     user_data["board"] = board_id
     user_data['name'] = board_name
     save_user_data(chat_id, user_data)
