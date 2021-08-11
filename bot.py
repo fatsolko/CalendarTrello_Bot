7a505ff32231a3dc183ef25a074c67531b449d24
Denis <Den2434358@gmail.com>
2021-08-11 19:17:23 +0300

    set_list fix

--- bot.py
+++ bot.py
@@ -201,7 +201,7 @@ def handle_message(message):
 
 def handle_reply(message):
     user_data = get_user_data(message.chat.id)
-    selected_list_id = '60b7b698530bfd733147435a'
+    selected_list_id = user_data['selected_list']['id']
     name_event = find_after(message.reply_to_message.text, " – ")
     url = "https://api.trello.com/1/cards?&key={}&token={}&name={}&desc={}&idList={}".format(
         trello_key,
