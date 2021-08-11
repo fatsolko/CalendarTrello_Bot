5a8f75134dfe9aa9bf7c8218f2d20399e75f7bd9
Denis <Den2434358@gmail.com>
2021-08-11 19:24:57 +0300

    fix

--- bot.py
+++ bot.py
@@ -215,8 +215,8 @@ def handle_reply(message):
     short_post_url = short_url.tinyurl.short(url)
     keyboard_send_trello = types.InlineKeyboardMarkup()
     name_selected_board = user_data['selected_board']["name"]
-    name_selected_list = user_data['selected']["selected_list"]
-    button_text = "Отправить на доску {}\nЛист {}".format(name_selected_board, )
+    name_selected_list = user_data['selected_board']["selected_list"]
+    button_text = "Отправить на доску {}\nЛист {}".format(name_selected_board,name_selected_list)
     callback_data = "send={}".format(short_post_url)
     url_button = types.InlineKeyboardButton(text=button_text, callback_data=callback_data)
     keyboard_send_trello.row(url_button)
